741b44474492ce832d461c4bae095dc5e68973ca
Irina <parakerta@mail.ru>
2022-11-23 17:00:05 +0300

    check sum with quantity

--- js/index.js
+++ js/index.js
@@ -1,31 +1,60 @@
-const checkboxes = document.getElementsByClassName("item_check");
+var checkboxes = document.getElementsByClassName("item_check");
+
 function selectAllItems(checked) {
   for (let checkbox of checkboxes) {
     checkbox.checked = checked;
   }
   calculateTotalCost();
 }
-function selectItem(checked,id) {
-    document.getElementById(id).checked = checked
-    calculateTotalCost();
-  }
+
+function selectItem(checked, id) {
+  document.getElementById(id).checked = checked;
+  calculateTotalCost();
+}
 
 function prettify(num) {
   var n = num.toString();
   return n.replace(/(\d{1,3}(?=(?:\d\d\d)+(?!\d)))/g, "$1" + " ");
 }
-function calculateTotalCost() {
-  var sum = 0;
+function calculatePrices() {
+  var counts = document.getElementsByClassName("item_count");
   let items__discount = document.getElementsByClassName("item-price__discount");
   var items__discount_array = [...items__discount].filter(
     (item) => item.tagName === "SPAN"
   );
-
+  var prices_discount = [];
   for (var i = 0; i < items__discount_array.length; i++) {
-   
+    prices_discount.push(
+      items__discount_array[i].innerHTML.replace(/\D/g, "") / counts[i].value
+    );
+  }
+  var items__nodiscount = document.getElementsByClassName(
+    "item-price__nodiscount"
+  );
+  var items__nodiscount_array = [...items__nodiscount].filter(
+    (item) => item.tagName === "SPAN"
+  );
+  var prices_nodiscount = [];
+  for (var i = 0; i < items__nodiscount_array.length; i++) {
+    prices_nodiscount.push(
+      items__nodiscount_array[i].innerHTML.replace(/\D/g, "") / counts[i].value
+    );
+  }
+  return [prices_discount, prices_nodiscount];
+}
+var [prices_discount, prices_nodiscount] = calculatePrices();
+function calculateTotalCost() {
+  var sum = 0;
+  var counts = document.getElementsByClassName("item_count");
+  let items__discount = document.getElementsByClassName("item-price__discount");
+  var items__discount_array = [...items__discount].filter(
+    (item) => item.tagName === "SPAN"
+  );
+  for (var i = 0; i < items__discount_array.length; i++) {
     if (checkboxes[i].checked) {
-        console.log(items__discount[i]);
-      sum += +items__discount_array[i].innerHTML.replace(/\D/g, "");
+        items__discount[i*2].innerHTML = prices_discount[i] * counts[i].value+ ' сом';
+        items__discount[i*2+1].innerHTML = prices_discount[i] * counts[i].value+ ' сом';
+      sum += prices_discount[i] * counts[i].value;
     }
   }
 
@@ -38,7 +67,9 @@ function calculateTotalCost() {
   );
   for (var i = 0; i < items__nodiscount_array.length; i++) {
     if (checkboxes[i].checked) {
-      sum_nodiscount += +items__nodiscount_array[i].innerHTML.replace(/\D/g, "");
+        items__nodiscount[i*2].innerHTML = prices_nodiscount[i] * counts[i].value + ' сом';
+      items__nodiscount[i*2+1].innerHTML = prices_nodiscount[i] * counts[i].value + ' сом';
+      sum_nodiscount += prices_nodiscount[i] * counts[i].value;
     }
   }
 
@@ -49,3 +80,5 @@ function calculateTotalCost() {
   document.getElementById("sum_discount").innerHTML =
     prettify(sum_discount) + " сом";
 }
+
+selectAllItems(true);
